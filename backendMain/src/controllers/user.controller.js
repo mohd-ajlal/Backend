62bea9390570b322f11b464d81f0629eba8ff6cd
Mohd Ajlal <ajlal00786@gmail.com>
2024-07-02 03:48:57 +0530

    Backend

--- backendMain/src/controllers/user.controller.js
+++ backendMain/src/controllers/user.controller.js
@@ -4,23 +4,51 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const generateAccessAndRefreshToken = async (UserId) => {
-  try {
-    const user = await User.findById(UserId);
-    const accessToken = user.generateAccessToken();
-    const refreshToken = user.generateRefreshToken();
+const generateAccessAndRefereshTokens = async (UserId) => {
+    try {
+        const user = await User.findById(UserId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
 
-    user.refreshToken = refreshToken;
-    await user.save({validateBeforeSave: false});
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
 
-    return {accessToken, refreshToken};
-  } catch (error) {
-    throw new ApiError(
-      500,
-      "Something went wrong while generating access token and refresh token"
-    );
-  }
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
 };
+
+// const generateAccessAndRefereshTokens = async (UserId) => {
+//     try {
+//         const user = await User.findById(UserId);
+//         console.log('User found:', user);
+
+//         if (!user) {
+//             console.error('User not found');
+//             throw new ApiError(404, "User does not exist");
+//         }
+
+//         console.log('Generating tokens...');
+//         const accessToken = user.generateAccessToken();
+//         const refreshToken = user.generateRefreshToken();
+//         console.log('Tokens generated:', { accessToken, refreshToken });
+
+//         user.refreshToken = refreshToken;
+//         await user.save({ validateBeforeSave: false });
+//         console.log('User saved with new refreshToken');
+
+//         return { accessToken, refreshToken };
+
+//     } catch (error) {
+//         console.error('Error in generateAccessAndRefereshTokens:', error);
+//         throw new ApiError(500, "Something went wrong while generating refresh and access token");
+//     }
+// };
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user detail from frontend
@@ -96,61 +124,89 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-const loginUser = asyncHandler(async (req, res) => {
-  // res boy -> data
-  // username or email
-  // find the user
-  // password check
-  // generate access and refresh token
-  // send cookie
+const loginUser = asyncHandler(async (req, res) =>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
 
-  const { email, username, password } = req.body;
+    const {email, username, password} = req.body
+    console.log(email);
 
-  if (!username || !email) {
-    throw new ApiError(400, "username or email is required");
-  }
-
-  const user = await User.findOne({
-    $or: [{ email }, { username }],
-  });
-
-  if (!user) {
-    throw new ApiError(404, "User not found");
-  }
-
-  const isPasswordValid = await user.isPasswordCorrect(password);
-
-  if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid user credentials");
-  }
-
-  const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
-
-  const loggedInUser =  await User.findById(user._id).select("-password -refreshToken");
-
-  const options = {
-    httpOnly: true,
-    secure:true
-  }
-
-  return res
-  .status(200)
-  .cookie("access_token", accessToken, options)
-  .cookie("refresh_token", refreshToken, options)
-  .json(
-    new ApiResponse(
-        200,
-        {
-            user: loggedInUser, accessToken, refreshToken
-        },
-        "User logged in successfully"
-    )
-  )
-});
-
-
-const logoutUser = asyncHandler(async(req, res) => {
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
     
+    // Here is an alternative of above code based on logic discussed in video:
+    // if (!(username || email)) {
+    //     throw new ApiError(400, "username or email is required")
+        
+    // }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+   const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
 })
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export { registerUser, loginUser, logoutUser};
